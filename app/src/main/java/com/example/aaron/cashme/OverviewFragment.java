8b009c2e8049de92427e2d5576e4a4fb852222ad
Aaron Zhong <aaron_zhong2b@hotmail.com>
2017-04-10 21:31:00 +1200

    add textview for netincome in overview page

--- app/src/main/java/com/example/aaron/cashme/OverviewFragment.java
+++ app/src/main/java/com/example/aaron/cashme/OverviewFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
  */
 public class OverviewFragment extends Fragment {
 
+    TextView netIncomeTextView;
 
     public OverviewFragment() {
         // Required empty public constructor
@@ -22,8 +24,16 @@ public class OverviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_overview, container, false);
+
+        netIncomeTextView = (TextView) root.findViewById(R.id.netIncomeTextView);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_overview, container, false);
     }
 
+    public void UpdateNetIncomeViewText(String newValue) {
+        netIncomeTextView.setText("$" + newValue);
+    }
 }
