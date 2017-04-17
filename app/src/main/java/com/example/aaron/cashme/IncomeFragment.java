0e2c3a70ceaa2f3bda09b859b0860e7e1fc20497
Aaron Zhong <aaron_zhong2b@hotmail.com>
2017-04-17 13:09:35 +1200

    add listview to income page, add adapter with dummy data to populate list

--- app/src/main/java/com/example/aaron/cashme/IncomeFragment.java
+++ app/src/main/java/com/example/aaron/cashme/IncomeFragment.java
@@ -1,24 +1,35 @@
 package com.example.aaron.cashme;
 
 
+import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class IncomeFragment extends Fragment {
+
+    ListView listView;
+    ListAdapter adapter;
+    DBHelper mydb;
 
     public IncomeFragment() {
         // Required empty public constructor
@@ -30,13 +41,17 @@ public class IncomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_income, container, false);
+        mydb = new DBHelper(getActivity());
 
         final Button optionWindow;
         final TextView amount;
         final EditText enteredAmount;
 
-
         amount = (TextView)root.findViewById(R.id.value);
+        listView = (ListView) root.findViewById(R.id.listView);
+
+        adapter = new ListAdapter(getActivity(), new String[] { "data1", "data2"});
+        listView.setAdapter(adapter);
 
         optionWindow = (Button) root.findViewById((R.id.plusButton));
         optionWindow.setTag(1);
