b52696bc05ea8b50fe5e5fa617630a3f036a9af6
Humbis <alex1041227101@gmail.com>
2017-04-26 14:51:28 +1200

    Changes made to main activity to remove unscoped block statements

--- app/src/main/java/com/example/aaron/cashme/MainActivity.java
+++ app/src/main/java/com/example/aaron/cashme/MainActivity.java
@@ -12,31 +12,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    fragment = new OverviewFragment();
-                    break;
-                case R.id.navigation_dashboard:
-                    fragment = new IncomeFragment();
-                    break;
-                case R.id.navigation_notifications:
-                    fragment = new ExpensesFragment();
-                    break;
-            }
-
-            final FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.replace(R.id.main_content, fragment).commit();
-            return true;
-        }
-
-    };
+//    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+//            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//
+//        @Override
+//        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//        }
+//
+//    };
 
     private Fragment fragment;
     private FragmentManager fragmentManager;
@@ -51,14 +37,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(this);
         fragmentManager = getSupportFragmentManager();
 
         final FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.main_content, new OverviewFragment()).commit();
 
-
-
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new OverviewFragment();
+                break;
+            case R.id.navigation_dashboard:
+                fragment = new IncomeFragment();
+                break;
+            case R.id.navigation_notifications:
+                fragment = new ExpensesFragment();
+                break;
+        }
+
+        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_content, fragment).commit();
+        return true;
+    }
 }
